e923d65b9f99eacc4ef1dd948ba7c4c9a9c21a82
Armend Lesi <contributor4@flatfeestack.io>
2022-04-10 13:48:06 +0200

    add real data to dashboard

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,22 +1,52 @@
 import * as React from 'react';
 import type { NextPage } from 'next';
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { useGetLoggingInformationQuery } from '../api/chaingate.api';
+import { LoggingInformationsResponseDto, PaymentHistory } from '../api/chaingate.generated';
+
+interface LoggingRow {
+    id: string;
+    paymentId: string;
+    priceAmount: number;
+    priceCurrency: string;
+    payAmount: number;
+    payCurrency: string;
+    actuallyPaid: number;
+    transaction: string;
+    state: string;
+    createdAt: Date;
+    updatedAt: Date;
+    webhook: string;
+}
 
 const Dashboard: NextPage = () => {
-    const rows = [
-        {id: "1", priceAmount: 100, priceCurrency: "USD", payAmount: 0.5, payCurrency: "ETH", actuallyPaid: 0.6, transaction: "alsdjkiin", state: "paid", time: new Date()},
-        {id: "2", priceAmount: 100, priceCurrency: "USD", payAmount: 0.5, payCurrency: "ETH", actuallyPaid: 0.6, transaction: "alsdjkiin", state: "paid", time: new Date()},
-        {id: "3", priceAmount: 100, priceCurrency: "USD", payAmount: 0.5, payCurrency: "ETH", actuallyPaid: 0.6, transaction: "alsdjkiin", state: "paid", time: new Date()},
-        {id: "4", priceAmount: 100, priceCurrency: "USD", payAmount: 0.5, payCurrency: "ETH", actuallyPaid: 0.6, transaction: "alsdjkiin", state: "paid", time: new Date()},
-        {id: "5", priceAmount: 100, priceCurrency: "USD", payAmount: 0.5, payCurrency: "ETH", actuallyPaid: 0.6, transaction: "alsdjkiin", state: "paid", time: new Date()},
-        {id: "6", priceAmount: 100, priceCurrency: "USD", payAmount: 0.5, payCurrency: "ETH", actuallyPaid: 0.6, transaction: "alsdjkiin", state: "paid", time: new Date()},
-        {id: "7", priceAmount: 100, priceCurrency: "USD", payAmount: 0.5, payCurrency: "ETH", actuallyPaid: 0.6, transaction: "alsdjkiin", state: "paid", time: new Date()},
-        {id: "8", priceAmount: 100, priceCurrency: "USD", payAmount: 0.5, payCurrency: "ETH", actuallyPaid: 0.6, transaction: "alsdjkiin", state: "paid", time: new Date()},
-        {id: "9", priceAmount: 100, priceCurrency: "USD", payAmount: 0.5, payCurrency: "ETH", actuallyPaid: 0.6, transaction: "alsdjkiin", state: "waiting", time: new Date()}
-    ];
+    const { data } = useGetLoggingInformationQuery({mode: "test"})
+    let rows: LoggingRow[] = [];
+
+    data?.forEach((payment: LoggingInformationsResponseDto) => {
+        payment.history?.forEach((history: PaymentHistory) => {
+            let row: LoggingRow = {
+                id: history.id,
+                paymentId: payment.payment_id,
+                priceAmount: history.price_amount,
+                priceCurrency: history.price_currency,
+                payAmount: history.pay_amount,
+                payCurrency: history.pay_currency,
+                actuallyPaid: history.actually_paid || 0,
+                transaction: "transaction here",
+                state: history.payment_state,
+                createdAt:  new Date(payment.created_at),
+                updatedAt: new Date(payment.updated_at),
+                webhook: payment.callback_url,
+            }
+            rows.push(row);
+        })
+
+    })
 
     const columns: GridColDef[] = [
-        { field: 'id', headerName: 'ID', width: 50 },
+        { field: 'id', headerName: 'ID', width: 150},
+        { field: 'paymentId', headerName: 'Payment ID', width: 300 },
         { field: 'priceAmount', headerName: 'Price Amount', width: 130 },
         { field: 'priceCurrency', headerName: 'Price Currency', width: 130 },
         { field: 'payAmount', headerName: 'Pay Amount', width: 130 },
@@ -24,7 +54,9 @@ const Dashboard: NextPage = () => {
         { field: 'actuallyPaid', headerName: 'Actually Paid', width: 130 },
         { field: 'transaction', headerName: 'Transaction', width: 160 },
         { field: 'state', headerName: 'State', width: 160 },
-        { field: 'time', headerName: 'Time', type: "dateTime", width: 160 },
+        { field: 'webhook', headerName: 'Webhook', width: 160 },
+        { field: 'createdAt', headerName: 'Created at', type: "dateTime", width: 160 },
+        { field: 'updatedAt', headerName: 'Updated at', type: "dateTime", width: 160 },
     ];
 
     return (
@@ -35,6 +67,17 @@ const Dashboard: NextPage = () => {
               columns={columns}
               pageSize={5}
               rowsPerPageOptions={[5]}
+              initialState={{
+                  sorting: {
+                      sortModel: [{ field: 'updatedAt', sort: 'desc' }],
+                  },
+                  columns: {
+                      columnVisibilityModel: {
+                          // Hide columns status and traderName, the other columns will remain visible
+                          id: false,
+                      },
+                  },
+              }}
           />
           </div>
       </>
