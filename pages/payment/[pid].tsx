b85d9813410abb34219ba253244e3788e0f4329e
Armend Lesi <32630865+a-lesi@users.noreply.github.com>
2022-06-12 17:20:30 +0200

    Update [pid].tsx

--- pages/payment/[pid].tsx
+++ pages/payment/[pid].tsx
@@ -195,7 +195,7 @@ function PartiallyPaid(props: { payAmount: BigNumber, stage: SocketMessage, fact
         <Grid item>
             <a style={{textDecoration: 'underline'}} target='_blank' href={getBlockExplorerURL(props.stage.body.payAddress, props.stage.body.currency, props.stage.body.mode)} rel="noreferrer">(Check Block Explorer)</a>
         </Grid>
-        <CopySnackbar isOpen={isSnackbarOpen} setIsOpen={setIsSnackbarOpen}></CopySnackbar>
+        <CopySnackbar isOpen={isSnackbarOpen} setIsOpen={setIsSnackbarOpen} content={"Successfully copied."}></CopySnackbar>
     </Grid>;
 }
 
